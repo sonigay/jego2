93d946904fe338520f4c00354b06d01fb58cf12d
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-17 09:50:16 +0900

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ['https://spreadsheets.google.com/feeds']
-creds = ServiceAccountCredentials.from_json_keyfile_name('jungsanfile-e5ae2dbc8879.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('jego-972d19158581.json', scope)
 client = gspread.authorize(creds)
 doc = client.open_by_url('https://docs.google.com/spreadsheets/d/15p6G4jXmHw7Z_iRCYeFwRzkzLxqf-3Pj0c6FeVuFYBM')
 
