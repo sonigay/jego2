a562ff4b33d9525e31e008ef1402bc038057b37c
sonigay <43840048+sonigay@users.noreply.github.com>
2021-07-17 19:08:14 +0900

    Update test.py

--- test.py
+++ test.py
@@ -33,14 +33,23 @@ async def on_message(message):
 	global gc #정산
 	global creds	#정산
     
-          
+
+	if message.content.startswith('!재고'):
+		SearchID = message.content[len('!재고')+1:]
+		gc = gspread.authorize(creds)
+		wks = gc.open('재고관리').worksheet('모델명다양성')
+		wks.update_acell('F2', '')
+		time.sleep(1)
+
 	if message.content.startswith('!재고'):
 		SearchID = message.content[len('!재고')+1:]
 		gc = gspread.authorize(creds)
 		wks = gc.open('재고관리').worksheet('구단위코드/재고출력')
+		wks2 = gc.open('재고관리').worksheet('모델명다양성')
 		wkstime = gc.open('재고관리').worksheet('재고데이터')
-		
+
 		wks.update_acell('A1', SearchID)
+		wks2.update_acell('F2', SearchID)
 		result = wks.acell('B1').value
 		result2 = wkstime.acell('A1').value
 		result3 = wks.acell('c1').value
